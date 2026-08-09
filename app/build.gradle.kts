8e39b2bb1ad9996bf7f84ed5bcfde4506564a457
Minjoon Lee <newdlakstod@gmail.com>
2026-08-09 22:31:42 +0900

    \ Brighten toolbar settings icon\

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         base.archivesName.set("G1_CHALKBOARD")
         minSdk = 26
         targetSdk = 35
-        versionCode = 18
-        versionName = "0.5.2"
+        versionCode = 19
+        versionName = "0.5.3"
     }
 
     compileOptions {
