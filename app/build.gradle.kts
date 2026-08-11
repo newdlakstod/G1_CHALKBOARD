775ff281eb715954f5e78594b0bc61010abbdf42
Minjoon Lee <newdlakstod@gmail.com>
2026-08-11 23:36:09 +0900

    fix: replace oversized default app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         base.archivesName.set("G1_SKETCHBOOK")
         minSdk = 26
         targetSdk = 35
-        versionCode = 43
-        versionName = "3.0.3"
+        versionCode = 44
+        versionName = "3.0.4"
     }
 
     compileOptions {
